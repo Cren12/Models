dd3d27abb94c339be72904af40ced44d44f4b384
Cren12 <38407014+Cren12@users.noreply.github.com>
2018-09-03 15:53:43 +0200

    Create EmptyTemplate.R

---
+++ EmptyTemplate.R
@@ -0,0 +1,28 @@
+packages <- c()
+
+# +------------------------------------------------------------------
+# | library and require load and attach add-on packages. Download and
+# | install packages from CRAN-like repositories.
+# +------------------------------------------------------------------
+
+lapply(X = packages,
+       FUN = function(package){
+         if (!require(package = package,
+                      character.only = TRUE))
+         {
+           install.packages(pkgs = package,
+                            repos = "https://cloud.r-project.org")
+           library(package = package,
+                   character.only = TRUE)
+         } else {
+           library(package = package,
+                   character.only = TRUE)    
+         }
+       })
+
+# +------------------------------------------------------------------
+# | source() causes R to accept its input from the named file or URL
+# | or connection or expressions directly.
+# +------------------------------------------------------------------
+
+# source()
