01b3b596c20b5d3ba3653f82df71ce6f023e63fc
Cren12 <38407014+Cren12@users.noreply.github.com>
2018-10-11 17:01:04 +0200

    Add files via upload

---
+++ MinAvgDD_SMA.R
@@ -0,0 +1,117 @@
+packages <- c('quantmod',
+              'foreach',
+              'doFuture')
+
+# +------------------------------------------------------------------
+# | library and require load and attach add-on packages. Download and
+# | install packages from CRAN-like repositories.
+# +------------------------------------------------------------------
+
+lapply(X = packages,
+       FUN = function(package){
+         if (!require(package = package,
+                      character.only = TRUE))
+         {
+           install.packages(pkgs = package,
+                            repos = "https://cloud.r-project.org")
+           library(package = package,
+                   character.only = TRUE)
+         } else {
+           library(package = package,
+                   character.only = TRUE)    
+         }
+       })
+
+# +------------------------------------------------------------------
+# | Sys.setenv sets environment variables.
+# +------------------------------------------------------------------
+
+Sys.setenv(TZ = 'UTC')
+
+# +------------------------------------------------------------------
+# | source() causes R to accept its input from the named file or URL
+# | or connection or expressions directly.
+# +------------------------------------------------------------------
+
+# source()
+
+# +------------------------------------------------------------------
+
+MinAvgDD_SMA <- function(
+  ohlc,
+  from.n = 5,
+  to.n = 250,
+  penalty = 4e-03
+)
+{
+  # +------------------------------------------------------------------
+  # | Form row and column sums and means for numeric arrays (or data 
+  # | frames). Conversion functions to coerce data objects of arbitrary
+  # | classes to class xts and back, without losing any attributes of
+  # | the original format. An S3 object of class xts is returned. 
+  # +------------------------------------------------------------------
+  
+  price <- reclass(x = rowMeans(ohlc),
+                   match.to = ohlc)
+  
+  # +------------------------------------------------------------------
+  # | Register the doFuture parallel adaptor to be used by the foreach 
+  # | package.
+  # +------------------------------------------------------------------
+  
+  registerDoFuture()
+  
+  # +------------------------------------------------------------------
+  # | This function allows the user to plan the future, more 
+  # | specifically, it specifies how future():s are resolved, e.g. 
+  # | sequentially or in parallel. If multicore evaluation is supported,
+  # | that will be used, otherwise multisession evaluation will be used.
+  # +------------------------------------------------------------------
+  
+  plan(multiprocess)
+  
+  sma.dd.table <- foreach(n = from.n:to.n, .combine = rbind.data.frame) %dopar%
+  {
+    # +------------------------------------------------------------------
+    # | Calculate various moving averages (MA) of a series. Simple moving
+    # | average is returned.
+    # +------------------------------------------------------------------
+    
+    sma <- SMA(x = price,
+               n = n)
+    
+    signal <- price >= sma
+    R <- ifelse(signal, lag(ClCl(ohlc), -1), 0) - (signal != lag(signal)) * penalty
+    
+    # +------------------------------------------------------------------
+    # | findDrawdowns will find the starting period, the ending period, 
+    # | and the amount and length of the drawdown.
+    # +------------------------------------------------------------------
+    
+    sma.dd.table <- (cbind(n, mean(Drawdowns(R = na.omit(R)))))
+    
+    colnames(sma.dd.table) <- c('n', 'AvgDrawdown')
+    return(sma.dd.table)
+  }
+  
+  # +------------------------------------------------------------------
+  # | lm is used to fit linear models.
+  # +------------------------------------------------------------------
+  
+  model <- lm(formula = sma.dd.table$AvgDrawdown ~ sma.dd.table$n + I(sma.dd.table$n ^ 2))
+  
+  # +------------------------------------------------------------------
+  # | Determines the location, i.e., index of the (first) maximum of a
+  # | numeric vector.
+  # +------------------------------------------------------------------
+  
+  best.n <- sma.dd.table$n[which.max(model$fitted.values)]
+  
+  # +------------------------------------------------------------------
+  # | Calculate various moving averages (MA) of a series. Simple moving
+  # | average is returned.
+  # +------------------------------------------------------------------
+  
+  return(as.numeric(last(SMA(x = price,
+                             n = best.n))))
+}
---
+++ TradingTestSuite.R
@@ -0,0 +1,355 @@
+package <- c("compiler",
+             "quantmod",
+             "plyr",
+             "devtools",
+             "PerformanceAnalytics")
+
+# +------------------------------------------------------------------
+# | library and require load and attach add-on packages. Download and
+# | install packages from CRAN-like repositories.
+# +------------------------------------------------------------------
+
+lapply(X = package,
+       FUN = function(this.package){
+         if (!require(package = this.package,
+                      character.only = TRUE))
+         {
+           install.packages(pkgs = this.package,
+                            repos = "https://cloud.r-project.org")
+           library(package = this.package,
+                   character.only = TRUE)
+         } else {
+           library(package = this.package,
+                   character.only = TRUE)    
+         }
+       })
+# install_github("braverock/FinancialInstrument")
+# install_github("braverock/blotter")
+# install_github("braverock/quantstrat")
+# install_github("braverock/PerformanceAnalytics")
+require(quantstrat)
+
+# +------------------------------------------------------------------
+# | source() causes R to accept its input from the named file or URL
+# | or connection or expressions directly.
+# +------------------------------------------------------------------
+
+source('WinDoPar.R')
+source('PerfectTimingIndicator_sig.R')
+
+# +------------------------------------------------------------------
+
+osTotSize <- function(
+  data, 
+  timestamp, 
+  orderqty, 
+  ordertype, 
+  orderside, 
+  portfolio,
+  symbol, 
+  ruletype, 
+  digits = 0,
+  acct.name,
+  ...
+)
+{
+  if (orderqty == "all" && !(ruletype %in% c("exit", "risk")) || 
+      orderqty == "trigger" && ruletype != "chain") {
+    stop(paste("orderqty 'all'/'trigger' would produce nonsense, maybe use osMaxPos instead?\n", 
+               "Order Details:\n", "Timestamp:", timestamp, "Qty:", 
+               orderqty, "Symbol:", symbol))
+  }
+  
+  # +------------------------------------------------------------------
+  # | The updatePortf function goes through each symbol and calculates
+  # | the PL for each period prices are available.
+  # +------------------------------------------------------------------
+  
+  updatePortf(Portfolio = portfolio,
+              Symbols = symbol)
+  
+  # +------------------------------------------------------------------
+  # | Constructs the equity account calculations from the portfolio 
+  # | data and corresponding close prices.
+  # +------------------------------------------------------------------
+  
+  updateAcct(name = acct.name)
+  
+  # +------------------------------------------------------------------
+  # | Calculates End.Eq and Net.Performance.
+  # +------------------------------------------------------------------
+  
+  updateEndEq(Account = acct.name)
+  
+  # +------------------------------------------------------------------
+  # | Get a portfolio object conssting of either a nested list 
+  # | (getPortfolio).
+  # +------------------------------------------------------------------
+  
+  portfolio.object <- blotter::getPortfolio(portfolio)
+  
+  # +------------------------------------------------------------------
+  # | Retrieves an account object from the .blotter environment. Useful
+  # | for local examination or charting, or storing interim results for
+  # | later reference.
+  # +------------------------------------------------------------------
+  
+  account <- getAccount(acct.name)
+  
+  equity <- as.numeric(account$summary$End.Eq[as.character(timestamp), ])
+  if (length(equity) == 0)
+  {
+    equity <- as.numeric(account$summary$End.Eq[1, ])
+  }
+  avail.liq <- equity - as.numeric(portfolio.object$summary$Gross.Value[as.character(timestamp), ])
+  if (length(avail.liq) == 0)
+  {
+    avail.liq <- equity
+  }
+  to.trade.value <- avail.liq
+  to.trade.value <- ifelse(to.trade.value > 0, min(c(avail.liq, to.trade.value)), to.trade.value)
+  to.trade.shares <- ifelse(to.trade.value >= 0, floor(to.trade.value / Cl(mktdata[timestamp, ])), ceiling(to.trade.value / Cl(mktdata[timestamp, ])))
+  orderqty <- to.trade.shares
+  return(orderqty)
+}
+
+# +------------------------------------------------------------------
+
+Symbols <- c("LQD",
+             "HYG",
+             "SPY",
+             "GLD",
+             "TLT",
+             "XLF",
+             "XLV",
+             "DIA")
+
+require(Rblpapi)
+require(foreach)
+con <- blpConnect()
+ts <- bdh(securities = paste(Symbols, 'US Equity'),
+          fields = c('PX_OPEN', 'PX_HIGH', 'PX_LOW', 'PX_LAST'),
+          start.date = Sys.Date() - 365* 30)
+foreach(i = 1:length(ts)) %do%
+{
+  ts.xts <- xts(x = ts[[i]][, -1],
+                order.by = ts[[i]][, 1])
+  colnames(ts.xts) <- c('Open', 'High', 'Low', 'Close')
+  assign(x = Symbols[i],
+         value = ts.xts)
+  return(Symbols[i])
+}
+
+# +------------------------------------------------------------------
+# | Functions to load and manage Symbols in specified environment. 
+# | Called for its side-effect with env set to a valid environment 
+# | and auto.assign=TRUE, getSymbols will load into the specified env
+# | one object for each Symbol specified, with class defined by 
+# | return.class.
+# +------------------------------------------------------------------
+
+getSymbols(Symbols = Symbols,
+           from = Sys.Date() - 365 * 30)
+
+name <- 'Trading'
+currency <- 'USD'
+initEq <- 10000 * length(Symbols)
+TxnFees <- -4
+Sys.setenv(TZ = 'UTC')
+
+# +------------------------------------------------------------------
+# | Remove the order_book, account, and portfolio of given name.
+# +------------------------------------------------------------------
+
+rm.strat(name = name)
+
+# +------------------------------------------------------------------
+# | Constructs and initializes a portfolio object, which is used to
+# | contain transactions, positions, and aggregate level values.
+# +------------------------------------------------------------------
+
+initPortf(name = name,
+          symbols = Symbols,
+          currency = currency)
+
+# +------------------------------------------------------------------
+# | Inputs portfolios: a list of portfolio object names to attach to
+# | the account. initDate: date prior to the first close price given,
+# | used to contain initial account equity and initial position.
+# | initEq: initial equity or starting capital, default is 100,000.
+# +------------------------------------------------------------------
+
+initAcct(name = name,
+         portfolios = name,
+         initEq = initEq)
+
+# +------------------------------------------------------------------
+# | This function sets up the order container by portfolio.
+# +------------------------------------------------------------------
+
+initOrders(portfolio = name,
+           symbols = Symbols)
+
+# +------------------------------------------------------------------
+# | All 'currency' instruments must be defined before instruments of
+# | other types may be defined.
+# +------------------------------------------------------------------
+
+currency(primary_id = currency)
+
+# +------------------------------------------------------------------
+# | Variables passed in dots will be added to the strategy object, 
+# | and may be used by initialization and wrapup functions, as well 
+# | as indicators, signals, and rules.
+# +------------------------------------------------------------------
+
+strategy(name = name,
+         store = TRUE)
+
+for(primary_id in Symbols)
+{
+  stock(primary_id = primary_id,
+        currency = currency)
+}
+
+# +------------------------------------------------------------------
+# | Indicators are typically standard technical or statistical
+# | analysis outputs, such as moving averages, bands, or pricing 
+# | models.
+# +------------------------------------------------------------------
+
+add.indicator(strategy = name,
+              name = 'volatility',
+              arguments = list(OHLC = quote(OHLC(mktdata)),
+                               n = 5,
+                               calc = 'yang.zhang',
+                               N = 3),
+              label = 'sigma',
+              store = TRUE)
+add.indicator(strategy = name,
+              name = 'WinDoPar',
+              arguments = list(x = quote(OHLC(mktdata)),
+                               n = 250,
+                               w = 'exp',
+                               fun = PTI,
+                               oversold.p = .5),
+              label = 'pti',
+              store = TRUE)
+
+# +------------------------------------------------------------------
+# | This adds a signal definition to a strategy object.
+# +------------------------------------------------------------------
+
+add.signal(strategy = name,
+           name = 'sigThreshold',
+           arguments = list(column = 'pti',
+                            threshold = .5,
+                            relationship = 'gte',
+                            cross = FALSE),
+           label = 'pti.buy',
+           store = TRUE)
+
+# +------------------------------------------------------------------
+# | Rules will be processed in a very particular manner, so it bears
+# | going over.
+# +------------------------------------------------------------------
+
+add.rule(strategy = name,
+         name = 'ruleSignal',
+         arguments = list(sigcol = 'pti.buy',
+                          sigval = TRUE,
+                          orderqty = 1,
+                          ordertype = 'market',
+                          orderside = 'long',
+                          replace = TRUE,
+                          osFUN = osTotSize,
+                          acct.name = name,
+                          TxnFees = TxnFees),
+         label = 'pti.buy.enter',
+         type = 'enter',
+         store = TRUE)
+add.rule(strategy = name,
+         name = 'ruleSignal',
+         arguments = list(sigcol = 'pti.buy',
+                          sigval = TRUE,
+                          orderqty = 'all',
+                          ordertype = 'stoplimit', # stoplimit # stoptrailing
+                          orderside = 'long',
+                          orderset = 'stop',
+                          threshold = quote(-mktdata[timestamp, 'X1.sigma']),
+                          tmult = TRUE,
+                          TxnFees = TxnFees),
+         label = 'pti.buy.chain',
+         type = 'chain',
+         parent = 'pti.buy.enter',
+         store = TRUE)
+
+# +------------------------------------------------------------------
+# | This function is the wrapper that holds together the execution of
+# | a strategy.
+# +------------------------------------------------------------------
+
+applyStrategy(strategy = name,
+              portfolios = name)
+
+# +------------------------------------------------------------------
+# | The updatePortf function goes through each symbol and calculates
+# | the PL for each period prices are available.
+# +------------------------------------------------------------------
+
+updatePortf(Portfolio = name,
+            Symbols = Symbols)
+
+# +------------------------------------------------------------------
+# | Constructs the equity account calculations from the portfolio 
+# | data and corresponding close prices.
+# +------------------------------------------------------------------
+
+updateAcct(name = name)
+
+# +------------------------------------------------------------------
+# | Calculates End.Eq and Net.Performance.
+# +------------------------------------------------------------------
+
+updateEndEq(Account = name)
+
+for (symbol in Symbols)
+{
+  dev.new()
+  
+  # +------------------------------------------------------------------
+  # | Produces a three-panel chart of time series charts that contains
+  # | prices and transactions in the top panel, the resulting position
+  # | in the second, and a cumulative profit-loss line chart in the 
+  # | third.
+  # +------------------------------------------------------------------
+  
+  try(chart.Posn(Portfolio = name,
+                 Symbol = symbol))
+}
+
+# +------------------------------------------------------------------
+# | This function (for now) calculates return on initial equity for 
+# | each instrument in the portfolio or portfolios that make up an 
+# | account. These columns will be additive to return on capital of 
+# | each portfolio, or of the entire account.
+# +------------------------------------------------------------------
+
+R <- PortfReturns(Account = name)
+
+R$Tot.DailyEqPl <- rowSums(R)
+
+# +------------------------------------------------------------------
+# | For a set of returns, create a wealth index chart, bars for 
+# | per-period performance, and underwater chart for drawdown.
+# +------------------------------------------------------------------
+
+charts.PerformanceSummary(R = R,
+                          ylog = TRUE,
+                          main = 'Trading performance')
+
+# +------------------------------------------------------------------
+# | Get the order book object.
+# +------------------------------------------------------------------
+
+getOrderBook(portfolio = name)
