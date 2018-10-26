fb896c8b1bded1a769a8fea638810c00bb75a033
Cren12 <38407014+Cren12@users.noreply.github.com>
2018-10-26 17:30:52 +0200

    Update ClassifyLastObs.R

--- ClassifyLastObs.R
+++ ClassifyLastObs.R
@@ -46,37 +46,47 @@ ClassifyLastObs <- function(
   y <- data$Minimum.point
   X <- data[, -1]
   
-  # +------------------------------------------------------------------
-  # | Performs a principal components analysis on the given data matrix
-  # | and returns the results as an object of class prcomp.
-  # +------------------------------------------------------------------
-  
-  pca <- prcomp(x = X,
-                scale. = TRUE)
-  
-  # +------------------------------------------------------------------
-  # | neuralnet is used to train neural networks. neuralnet returns an
-  # | object of class nn.
-  # +------------------------------------------------------------------
-  
-  nnet <- neuralnet(formula = y ~ PC1 + PC2 + PC3,
-                    data = cbind.data.frame(y, pca$x[, 1:3]),
-                    hidden = 2,
-                    linear.output = FALSE)
-  
-  ## Complete model:
-  # nnet1 <- neuralnet(formula = as.formula(substr(paste('y ~', paste(colnames(X), '+ ', collapse = '')), 1, nchar(paste('y ~', paste(colnames(X), '+ ', collapse = ''))) - 3)),
-  #                    data = cbind.data.frame(y, X),
-  #                    hidden = 2)
-  
-  # +------------------------------------------------------------------
-  # | compute, a method for objects of class nn, typically produced by
-  # | neuralnet. Computes the outputs of all neurons for specific 
-  # | arbitrary covariate vectors given a trained neural network.
-  # +------------------------------------------------------------------
-  
-  out <- compute(x = nnet,
-                 covariate = pca$x[, 1:3])
-  
-  return(as.numeric(last(out$net.result)))
+  out <- tryCatch(
+    expr = {
+      
+      # +------------------------------------------------------------------
+      # | Performs a principal components analysis on the given data matrix
+      # | and returns the results as an object of class prcomp.
+      # +------------------------------------------------------------------
+      
+      pca <- prcomp(x = X,
+                    scale. = TRUE)
+      
+      # +------------------------------------------------------------------
+      # | neuralnet is used to train neural networks. neuralnet returns an
+      # | object of class nn.
+      # +------------------------------------------------------------------
+      
+      nnet <- neuralnet(formula = y ~ PC1 + PC2 + PC3 + PC4,
+                        data = cbind.data.frame(y, pca$x[, 1:4]),
+                        hidden = 2,
+                        linear.output = FALSE)
+      
+      ## Complete model:
+      # nnet1 <- neuralnet(formula = as.formula(substr(paste('y ~', paste(colnames(X), '+ ', collapse = '')), 1, nchar(paste('y ~', paste(colnames(X), '+ ', collapse = ''))) - 3)),
+      #                    data = cbind.data.frame(y, X),
+      #                    hidden = 2)
+      
+      # +------------------------------------------------------------------
+      # | compute, a method for objects of class nn, typically produced by
+      # | neuralnet. Computes the outputs of all neurons for specific 
+      # | arbitrary covariate vectors given a trained neural network.
+      # +------------------------------------------------------------------
+      
+      out <- compute(x = nnet,
+                     covariate = pca$x[, 1:4])
+      
+      return(as.numeric(last(out$net.result)))
+    },
+    error = function(e){
+      return(0)
+    },
+    finally = {}
+  )
+  return(out)
 }
