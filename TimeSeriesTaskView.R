f891941c39328e3e1340e9b602ee5eb4c9946758
Cren12 <38407014+Cren12@users.noreply.github.com>
2018-12-10 17:14:28 +0100

    Add files via upload

--- TimeSeriesTaskView.R
+++ TimeSeriesTaskView.R
@@ -1,5 +1,7 @@
-packages <- c('forecast',
-              'robets')
+packages <- c('robets',
+              'forecast',
+              'prophet',
+              'smooth')
 
 # +------------------------------------------------------------------
 # | library and require load and attach add-on packages. Download and
@@ -36,51 +38,145 @@ source('SmoothTimeSeries.R')
 
 # +------------------------------------------------------------------
 
-# stats::supsmu
-SupsmuTrading <- function(ohlc)
-{
-  super.sm <- supsmu(x = as.numeric(index(ohlc)),
-                     y = log(Cl(ohlc)))
-  super.sm <- xts(x = super.sm$y,
-                  order.by = index(ohlc))
-  value <- super.sm %>%
-    diff() %>%
-    last() %>%
-    as.numeric()
-  return(value)
-}
-
-# forecast::arfima
-ArfimaTrading <- function(ohlc, h)
+# robets::robets
+# Si ottiene una serie stazionaria con degli spike potenzialmente
+# lavorabili. Meno utile come indicatore di momentum ma da provare.
+RobetsIndicator <- function(ohlc, h)
 {
   fcast <- ohlc %>%
-    Cl() %>%
+    rowMeans() %>%
+    reclass(match.to = ohlc) %>%
     log() %>%
-    SmoothTimeSeries(spline = FALSE) %>%
-    arfima(drange = c(0, .99)) %>%
-    forecast(h = h, level = 99)
-  value <- cbind(fcast$lower, fcast$upper) %>%
-    last() %>%
+    as.vector() %>%
+    robets() %>%
+    forecast(h = h)
+  fcast$mean %>%
     as.numeric() %>%
-    exp()
-  return(value)
+    diff() %>%
+    last() %>%
+    return()
 }
 
-# robets::
-# MAPA::
+# prophet::prophet
+ProphetOscillator <- function(ohlc)
+{
+  price <- ohlc %>%
+    rowMeans() %>%
+    reclass(match.to = ohlc) %>%
+    log()
+  history <- data.frame(ds = index(ohlc),
+                        y = price)
+  m <- prophet(history)
+  future <- make_future_dataframe(m = m,
+                                  periods = 1)
+  forecast <- predict(m, future)
+  yhat <- forecast$yhat %>%
+    xts(order.by = as.Date(forecast$ds))
+  residuals <- (price - yhat) %>%
+    scale() %>%
+    last() %>%
+    as.numeric() %>%
+    return
+}
 
-ohlc <- `TLT US Equity`
-ohlc <- `GLD US Equity`
-
-
-
+ohlc <- `TLT US Equity`['2008/']
+ohlc <- `HYG US Equity`
+ohlc <- `SPY US Equity`
 
 ciao <- WinDoPar(x = ohlc,
-                 n = 250, 
+                 n = 250,
                  w = 'exp',
-                 fun = SupsmuTrading)
+                 par = FALSE,
+                 fun = ProphetOscillator)
+
 chartSeries(ohlc)
-addTA(ciao, col = 'deepskyblue')
-
-
+addTA(ta = ciao, col = 'deepskyblue', on = 1)
+addTA(ta = price - ciao * .9, col = 'coral')
+addTA(ta = quant, on = 2)
+zoomChart('2016/')
 chart.CumReturns(ClCl(ohlc) * ifelse(ciao > 0, 1, 0))
+
+
+
+### REPOSITORY ###
+
+# GAS::UniGASFit
+# UniGASFitIndicator <- function(ohlc)
+# {
+#   price <- ohlc %>%
+#     rowMeans() %>%
+#     reclass(match.to = ohlc)
+#   cl.cl <- ohlc %>%
+#     ClCl() %>%
+#     na.omit()
+#   op.op <- na.omit(Op(ohlc) / lag(Op(ohlc)) - 1)
+#   op.cl <- ohlc %>%
+#     OpCl() %>%
+#     na.omit()
+#   cl.op <- na.omit(Op(ohlc) / lag(Cl(ohlc)) - 1)
+#   GASSpec <- UniGASSpec(Dist = 'norm',
+#                         GASPar = list(location = TRUE,
+#                                       scale = TRUE,
+#                                       skewness = TRUE,
+#                                       shape = TRUE,
+#                                       shape2 = TRUE))
+#   fit.clcl <- UniGASFit(GASSpec = GASSpec,
+#                         data = cl.cl)
+#   fit.opop <- UniGASFit(GASSpec = GASSpec,
+#                         data = op.op)
+#   fit.opcl <- UniGASFit(GASSpec = GASSpec,
+#                         data = op.cl)
+#   fit.clop <- UniGASFit(GASSpec = GASSpec,
+#                         data = cl.op)
+#   sigma.clcl <- fit.clcl@Estimates$Moments[, 2] %>%
+#     sqrt() %>%
+#     last() %>%
+#     as.numeric()
+#   sigma.opop <- fit.opop@Estimates$Moments[, 2] %>%
+#     sqrt() %>%
+#     last() %>%
+#     as.numeric()
+#   sigma.opcl <- fit.opcl@Estimates$Moments[, 2] %>%
+#     sqrt() %>%
+#     last() %>%
+#     as.numeric()
+#   sigma.clop <- fit.clop@Estimates$Moments[, 2] %>%
+#     sqrt() %>%
+#     last() %>%
+#     as.numeric()
+#   if (last(abs(cl.cl)) > 2 * sigma.clcl ||
+#       last(abs(op.op)) > 2 * sigma.opop ||
+#       last(abs(op.cl)) > 2 * sigma.opcl ||
+#       last(abs(cl.op)) > 2 * sigma.clop)
+#   {
+#     last(price) %>%
+#       as.numeric() %>%
+#       return()
+#   } else {
+#     return(NA)
+#   }
+# }
+# 
+# UniGASFairValue <- function(ohlc)
+# {
+#   price <- ohlc %>%
+#     rowMeans() %>%
+#     reclass(match.to = ohlc)
+#   fv <- WinDoPar(x = ohlc,
+#                  n = 250,
+#                  w = 'exp',
+#                  fun = UniGASFitIndicator)
+#   for (i in 2:nrow(fv))
+#   {
+#     if (is.na(fv[i, ]))
+#       fv[i, ] <- fv[i - 1, ]
+#   }
+#   discount <- (price / fv - 1) * 100
+#   quant <- WinDoPar(x = discount,
+#                     n = 250,
+#                     w = 'exp',
+#                     fun = quantile,
+#                     probs = .05,
+#                     na.rm = TRUE)
+#   return(quant)
+# }
